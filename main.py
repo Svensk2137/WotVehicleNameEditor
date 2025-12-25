c7f6eef5b6c27527309a33d791c634de0140c011
Svensk2137 <kubadmidm@wp.pl>
2025-12-25 19:35:00 +0100

    idk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3.14
+import os
 import tkinter as tk
-from tkinter import filedialog
+from pydoc import text
+from tkinter import filedialog, messagebox, ttk
+
+import polib
 
 
 class App(tk.Tk):
@@ -8,26 +12,67 @@ class App(tk.Tk):
         super().__init__()
         self.title("Wot Vehicle name changer")
         self.geometry("500x300")
-        self.create_widgets()
 
-        self.lcFolder = ""
+        self.excluded_strings = [
+            "Chassis_",
+            "Turret_",
+            "_descr",
+            "_long_special",
+            "_short",
+            "_short_special",
+        ]
 
-    def create_widgets(self):
-        filepickerbutton = tk.Button(self, text="Pick file", command=self.pickFolder)
-        filepickerbutton.pack()
-        self.pathLabel = tk.Label(
-            self, text="Choose folder <WOT main dir>/res/text/lc_messages/"
-        )
-        self.pathLabel.pack()
-
-    def pickFolder(self):
-        lcfolder = filedialog.askdirectory(
+        print("Ask for lc_messages folder")
+        self.lcfolder = filedialog.askdirectory(
             title="Select lc_messages folder",
             initialdir="/drives/Gaming/SteamLibrary/steamapps/common/World of Tanks/eu/res/text/",
         )
-        self.pathLabel["text"] = lcfolder
-        # wottext = polib.mofile(mofile)
-        self.lcFolder = lcfolder
+
+        print("Get *_vehicles.mo files")
+        self.textFiles = []
+        for entry in os.listdir(self.lcfolder):
+            if "_vehicles.mo" in entry:
+                self.textFiles.append(entry)
+
+        print(self.textFiles)
+        if self.textFiles == []:
+            messagebox.showerror("Huh?", "Picked a wrong folder or canceled")
+            exit()
+
+        self.optionVarNation = tk.StringVar(self)
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.nationOptions = ttk.OptionMenu(
+            self,
+            self.optionVarNation,
+            self.textFiles[0],
+            *self.textFiles,
+            command=self.populateVehicles,
+        )
+        self.nationOptions.pack()
+
+        self.vehicleOptions = ttk.OptionMenu(self, self.optionVarNation, "")
+        self.vehicleOptions.pack()
+
+        ttk.Label(self, text="Vehicle name").pack()
+        self.tankName = ttk.Entry(self)
+        self.tankName.pack()
+
+    def populateVehicles(self, *args):
+        menu = self.vehicleOptions["menu"]
+        menu.delete(0, "end")
+        path = self.lcfolder + "/" + self.optionVarNation.get()
+        print(path)
+        list = polib.mofile(path)
+        vehicles = []
+        for entry in list:
+            if entry.msgid and not any(
+                excluded in entry.msgid for excluded in self.excluded_strings
+            ):
+                print(entry)
+                vehicles.append(entry.msgid)
+        print(vehicles)
 
 
 # vehicleName = input("Write the name of the vehicle: ")
